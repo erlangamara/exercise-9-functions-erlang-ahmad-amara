5cdaefa46e50273cfca20ca3552a09da70968251
erlangamara <erlang@Erlangs-MacBook-Pro.local>
2019-12-19 13:03:26 +0700

    upload tugas

---
+++ exercise-9-functions-erlang-ahmad-amara.js
@@ -0,0 +1,31 @@
+//Number 1
+function shoutOut(){
+    return 'Halo Function!';
+}
+
+console.log(shoutOut());
+
+//Number 2
+/*function calculateMultiply(num1, num2){
+    return num1 * num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);*/
+
+//Number 3
+/*function processSentence(name,age,adress,hobby){
+    sentence = `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${adress}, dan saya punya hobby yaitu ${hobby}!`;
+    return sentence;
+}
+
+var name = 'Agus';
+var age = 30;
+var adress = 'Jln. Malioboro, Yogyakarta';
+var hobby = 'gaming';
+
+var fullSentence = processSentence(name, age, adress, hobby);
+console.log(fullSentence);*/
